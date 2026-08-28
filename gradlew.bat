8f323f66410742ad332d7c70611d6e7c137e4062
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-08-28 14:21:53 +0200

    build: remove unusable Gradle wrapper script

--- gradlew.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist "%~dp0gradle\wrapper\gradle-wrapper.jar" (
-  java -classpath "%~dp0gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
-) else (
-  echo gradle-wrapper.jar is not vendored. Use the Gradle version declared in gradle\wrapper\gradle-wrapper.properties directly.
-  exit /b 1
-)
