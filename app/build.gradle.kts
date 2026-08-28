4052a7c29083e83094871248764f7411a11c98f9
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-08-28 23:52:11 +0200

    Bump icon asset hotfix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.stkmn.shareparser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.3.1"
+        versionCode = 8
+        versionName = "0.3.2"
     }
 
     compileOptions {
