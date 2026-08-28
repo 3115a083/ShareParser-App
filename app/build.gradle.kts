edd8ccc64b790e20c541f0177f9daa22979d077b
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-08-28 10:43:03 +0200

    Bump app version for workflow update testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.stkmn.shareparser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
     }
 
     compileOptions {
