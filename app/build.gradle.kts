91ec33f5a6c1499a66d9a4856766b542f001ee48
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-08-29 15:29:15 +0200

    Bump test build to 0.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.stkmn.shareparser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.3.5"
+        versionCode = 12
+        versionName = "0.3.6"
     }
 
     compileOptions {
