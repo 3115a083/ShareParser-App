f97843c0069eb2a2d9a74e4a0ad0c48e5e92681a
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-08-29 14:41:00 +0200

    Disable unreliable launcher icon switching

--- app/src/main/java/cc/stkmn/shareparser/LauncherIconManager.kt
+++ app/src/main/java/cc/stkmn/shareparser/LauncherIconManager.kt
@@ -3,8 +3,11 @@ package cc.stkmn.shareparser
 import android.content.Context
 import cc.stkmn.shareparser.data.LauncherIcon
 
+/**
+ * Launcher icon switching was removed because several Android launchers cached
+ * activity aliases unreliably. ShareParser now uses logo 3 exclusively.
+ */
 object LauncherIconManager {
-    @Suppress("UNUSED_PARAMETER")
     fun apply(context: Context, selected: LauncherIcon) = Unit
 
     fun normalize(icon: LauncherIcon): LauncherIcon = LauncherIcon.LOGO_3
