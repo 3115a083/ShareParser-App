757495bd133bca806d1e830f9be4050299d37db1
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-03 12:44:17 +0200

    Use localized Text wrapper across overhauled UI

--- app/src/main/java/cc/stkmn/shareparser/ui/SettingsHomeScreen.kt
+++ app/src/main/java/cc/stkmn/shareparser/ui/SettingsHomeScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
