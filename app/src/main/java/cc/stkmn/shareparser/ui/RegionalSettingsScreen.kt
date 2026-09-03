8bc32886450d1967ea26d177faf4ef6fe706588a
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-03 12:44:32 +0200

    Use localized Text wrapper across overhauled UI

--- app/src/main/java/cc/stkmn/shareparser/ui/RegionalSettingsScreen.kt
+++ app/src/main/java/cc/stkmn/shareparser/ui/RegionalSettingsScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
