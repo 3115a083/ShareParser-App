ffd727e2e96a48d15655ea1173c3460116872564
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-03 12:51:14 +0200

    Bump UI overhaul build to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.stkmn.shareparser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.3.7"
+        versionCode = 14
+        versionName = "0.4.0"
     }
 
     compileOptions {
