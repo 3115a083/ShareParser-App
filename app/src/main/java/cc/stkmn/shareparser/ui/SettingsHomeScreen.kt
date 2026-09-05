c4bc0cb94eacdd9c3e4fcfb629f6f1caf82fda57
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-06 01:32:12 +0200

    Import width for settings switch spacing

--- app/src/main/java/cc/stkmn/shareparser/ui/SettingsHomeScreen.kt
+++ app/src/main/java/cc/stkmn/shareparser/ui/SettingsHomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
