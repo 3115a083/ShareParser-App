583da2f7ae8fa87eed9d1f123b3efd0ef5390b4d
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-06 00:51:17 +0200

    Fix settings control imports

--- app/src/main/java/cc/stkmn/shareparser/ui/SettingsHomeScreen.kt
+++ app/src/main/java/cc/stkmn/shareparser/ui/SettingsHomeScreen.kt
@@ -26,6 +26,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
